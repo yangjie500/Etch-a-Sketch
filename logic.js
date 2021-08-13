bb712b7631db0b1ae587062c0f184d8f77eef18d
Teh Yang Jie <kristeh97@gmail.com>
2021-08-13 23:24:42 +0800

    added more logic.js

--- logic.js
+++ logic.js
@@ -6,24 +6,33 @@
         div.classList.add('grid-child');
         gridContainer.appendChild(div);
     }
-    console.log('helol');
 })();
 
 function getSliderValue(e) {
     return e.target.value;
 }
+
 function getColorHex(e) {
     return e.target.value;
 }
+
+function updateGridCss(gridContainer, dim) {
+    gridContainer.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
+    console.log(typeof dim);
+}
+
 function removeChild(gridContainer) {
     let childArr = Array.from(gridContainer.children);
     for (let i of childArr) {
         i.remove(); //or gridContainer.removeChild(i);
     }
 }
+
 function makeGrid(e) {
     const gridContainer = document.querySelector('.grid-container');
     removeChild(gridContainer);
+    updateGridCss(gridContainer, getSliderValue(e));
     for (let i=1; i<=getSliderValue(e) * getSliderValue(e); i++) {
         let div = document.createElement('div');
         div.classList.add('grid-child');
@@ -32,13 +41,56 @@ function makeGrid(e) {
     
 }
 
+function generateRainbow() {
+    x = Math.floor(Math.random()* 360);
+    y = Math.floor(Math.random()* 100);
+    z = Math.floor(Math.random()* 100);
+    return [x, y, z];
+    
+}
+
+function pickOneMode(e) {
+    const gridChildren = document.querySelectorAll('.grid-child');
+    className = e.target.classList[0];
+    if (className === 'to-black') {
+        for (const i of gridChildren) {
+            i.addEventListener('mouseover', function(e) {
+            e.target.classList.add('black');
+            });
+        };
+
+    } else if (className === 'to-rainbow') {
+        for (const i of gridChildren) {
+            i.addEventListener('mouseover', function(e) {
+                let color = generateRainbow()
+                e.target.style.backgroundColor = `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+            })
+        };
+    } else {
+        for (const i of gridChildren) {
+            i.addEventListener('mouseover', function(e) {
+                e.target.classList.remove('black');
+                e.target.style.backgroundColor = `#fff`;a
+            })
+        }
+    }
+}
+
 
 function sketch () {
     const slider = document.querySelector(`input[type='range']`);
     const colorPicker = document.querySelector(`input[type='color']`);
+    const gridChildren = document.querySelectorAll('.grid-child');
+    const buttons = document.querySelectorAll("button");
     
     slider.addEventListener('change', makeGrid);
     colorPicker.addEventListener('input', getColorHex);
+    for (const i of buttons) {
+        i.addEventListener('click', pickOneMode);
+    }
+
+
+
 }
 
 window.onload = sketch();
